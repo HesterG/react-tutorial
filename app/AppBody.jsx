b13b7f3d18d5c20b109567f678d623e60a996a9b
HesterG <hestergong@gmail.com>
2017-05-05 16:51:29 -0400

    Step 3

--- app/AppBody.jsx
+++ app/AppBody.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import Completed from './Completed'
+import Step from './Step'
 
 export default class AppBody extends Component {
 	render() {
 		return (
 			<div className="step">
-				Step 3
+				<Step/>
+				<Completed/>
 			</div>
 		)
 	}
 }
-
