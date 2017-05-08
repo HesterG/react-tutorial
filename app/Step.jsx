f3668a7bc5cbdb2f474783a254effe68cc2e6319
HesterG <hestergong@gmail.com>
2017-05-08 16:06:13 -0400

    Update Step.jsx

--- app/Step.jsx
+++ app/Step.jsx
@@ -4,10 +4,9 @@ export default class Step extends Component {
   render() {
     let stepNumber = this.props.stepNumber
     return (
-      <span className="step">
+      <span className="step" onClick={this.props.increaseStep}>
         Step {stepNumber}&nbsp;
       </span>
     )
   }
 }
-
